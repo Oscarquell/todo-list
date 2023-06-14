43384cd524a86ea748db9bee17b7050e31cb899b
Oscarquell <oscarquell96@gmail.com>
2023-06-15 02:23:54 +0600

    Added reducers for add button, and delete with copy btn

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,8 +1,14 @@
 import rootReducer from "./reducers/reducers";
-import {createStore} from "redux";
+import {applyMiddleware, compose, legacy_createStore as createStore} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
 const initialState = {}
 
-const store = createStore(rootReducer(), initialState)
+const composeFunc = process.env.NODE_ENV === 'development' ? composeWithDevTools : compose
+
+const composeEnhancers = composeFunc(applyMiddleware(thunk))
+
+const store = createStore(rootReducer(), initialState, composeEnhancers)
 
 export default store;
